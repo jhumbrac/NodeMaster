39f037fe8270f45943383d0471b931b4c254d400
Michael Robil <ch.michaelsamir@hotmail.com>
2019-12-19 14:23:56 -0600

    db created and table added

--- models/example.js
+++ models/example.js
@@ -1,7 +1,34 @@
 module.exports = function(sequelize, DataTypes) {
-  var Example = sequelize.define("Example", {
-    text: DataTypes.STRING,
-    description: DataTypes.TEXT
+  const Characters = sequelize.define("Character", {
+    name: DataTypes.STRING,
+    class: DataTypes.STRING,
+    race: DataTypes.STRING,
+    str: DataTypes.INTEGER,
+    dex: DataTypes.INTEGER,
+    con: DataTypes.INTEGER,
+    int: DataTypes.INTEGER,
+    wis: DataTypes.INTEGER,
+    chs: DataTypes.INTEGER,
+    hp: DataTypes.INTEGER,
+    xp: DataTypes.INTEGER,
+    ac: DataTypes.INTEGER
   });
-  return Example;
+  return Characters.create({
+    name: "torgar",
+    class: "barbarian",
+    race: "human",
+    str: 16,
+    dex:14,
+    con: 15,
+    int: 9,
+    wis: 13,
+    chs: 11,
+    hp: 28,
+    xp:0,
+    ac:14 
+  }
+).then ( result => console.log(result) );
 };
+
+
+  
