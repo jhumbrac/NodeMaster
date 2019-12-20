3e92cbec3819023680be1cc857860dd71665e5db
jhumbrac <john.humbracht@generalassemb.ly>
2019-12-20 10:48:47 -0600

    roll.js added

---
+++ public/js/rolls.js
@@ -0,0 +1,13 @@
+// This page needs to have mathmatical formulas for the dice rolls. I'll list variables here for the different formulas we need, but basically the first number is the number of rolls and the second number is the sides of the die, so 2d6 would be the equivalent of rolling two six-sided dice. --> for the dice do a Math.random function between 1 and the second number.
+
+//I think we should try to make the number of dice rolls an argument that can be passed, so if we need to roll 3d6 that we can use the same function as 2d6 or 10d6
+
+// 1d4
+
+// 1d6
+
+// 1d8
+
+// 1d12
+
+// 1d20
