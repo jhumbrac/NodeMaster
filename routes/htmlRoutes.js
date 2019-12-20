e135ae9c1976f31646888aaf0bbd156995043d00
Michael Robil <ch.michaelsamir@hotmail.com>
2019-12-20 12:09:53 -0600

    database name in handlebars updated

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,19 +3,19 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+    db.Characters.findAll({}).then(function(data) {
       res.render("index", {
         msg: "Welcome!",
-        examples: dbExamples
+        characters: data
       });
     });
   });
 
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+    db.Characters.findOne({ where: { id: req.params.id } }).then(function(data) {
       res.render("example", {
-        example: dbExample
+        characters: data
       });
     });
   });
