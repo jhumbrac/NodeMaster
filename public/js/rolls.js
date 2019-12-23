79b2d4ad7caea43dc85caf7ae7ceec6bf868e917
Zack White <zrwhite2993@gmail.com>
2019-12-23 12:25:02 -0600

    Finished basic dice roll logic.

--- public/js/rolls.js
+++ public/js/rolls.js
@@ -1,51 +1,64 @@
-/*function initDiceRoll() {
-  const result = Math.floor(Math.random() * 7);
-  if (result >= 4) {
-    console.log('Holy shit that thing is big. You start with a disadvantage.')
-  } else if (result < 4) {
-    console.log(`${monster} is smaller than you. You start with advantage.`)
-  }
-  return result
-};*/
-
 let enemy = {
-  armorClass: 5,
-  health: 200
+  armorClass: 10,
+  attackPower: 25,
+  health: 200,
+  displayTitle: 'Enemy'
 }
 
 let player = {
   armorClass: 5,
-  health: 100
+  attackPower: 15,
+  health: 100,
+  displayTitle: 'Player'
 }
 
 var hit;
 var modifier = 0;
 
-function rollToHit(opponent) {
-  let hitRoll = Math.floor(Math.random() * 21 - modifier);
-  if (hitRoll > opponent.armorClass) {
-    hit = true;
-  } else {
-    hit = false;
-  }
-  return hit;
+function determineInitiative() {
+  let result = Math.floor(Math.random() * 100) > 50;
+  result ? console.log('Holy shit that thing is big. You start with a disadvantage.') : 
+  console.log(`Your enemy is smaller than you. You start with advantage.`);
+  return result;
 }
 
-function attack(opponent) {
-  var newVar = rollToHit();
-  if (newVar) {
-    var successfulHit = Math.floor(Math.random() * 21);
-    opponent.health -= successfulHit;
-    console.log(`You have hit for ${successfulHit}`);
-    console.log('Health: ', opponent.health)
-    if (opponent.health <= 0) {
-      console.log("Your enemy has been slain!")
-    }
-  } else {
-    console.log('You have missed!');
-  } 
+function rollDoesHit(opponent) {
+    return opponent.armorClass < Math.floor(Math.random() * 21 - modifier);
 }
-attack(enemy);
+
+function attack(attacker, opponent) {
+    if(rollDoesHit(opponent)) {
+      let damageRoll = Math.floor(Math.random() * attacker.attackPower);
+      opponent.health -= damageRoll;
+      console.log(`${attacker.displayTitle} has hit for ${damageRoll}`);
+      console.log('Health: ', opponent.health);
+      if (opponent.health <= 0) {
+        console.log(`${opponent.displayTitle} has been slain!`);
+      }
+    } else {
+      console.log(`${attacker.displayTitle} has missed.`)
+    }
+};
+
+let playerGoesFirst = determineInitiative();
+let attacker = playerGoesFirst ? player : enemy;
+let opponent = playerGoesFirst ? enemy : player;
+
+function handleAttack () {
+  attack(attacker, opponent);
+  let temp = opponent;
+  opponent = attacker;
+  attacker = temp;
+}
+
+const timer = setInterval(function () {
+  handleAttack();
+  if(attacker.health < 0 || opponent.health < 0){
+    clearInterval(timer);
+  }
+}, 1000);
+
+
 
 
 
