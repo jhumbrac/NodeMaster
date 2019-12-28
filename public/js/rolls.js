2eb00bb696942632dbc5ab4baf29889781574f58
jhumbrac <john.humbracht@generalassemb.ly>
2019-12-28 14:06:11 -0600

    uploading work 12/28

--- public/js/rolls.js
+++ public/js/rolls.js
@@ -1,69 +1,96 @@
 let enemy = {
-  armorClass: 10,
+  ac: 10,
+  dex:12,
   attackPower: 25,
-  health: 200,
+  hp: 200,
   displayTitle: 'Enemy'
 };
 let player = {
-  armorClass: 5,
+  ac: 5,
+  dex:15,
   attackPower: 15,
-  health: 100,
+  hp: 100,
   displayTitle: 'Player'
 };
-var hit;
-var modifier = 0;
 
-function rollDice(tempDice) {
-  let tempDice = '2d6';
-  let regexp = /(?<num>[0-9])(?:d)(?<sided>[0-9])/;
-  let num;
-  let sided;
-  let roll = Math.floor(Math.random());
-  
-}
+let attacker = player;
+let defender = enemy;
+let hit = false;
+let opponent; // don't remember what I was thinking here?
+let modifier = 0;
+let tempDice = '3d8';
+let roll = 0;
 
-function determineInitiative() {
-  let result = Math.floor(Math.random() * 100) > 50;
-  result ? console.log('Holy shit that thing is big. You start with a disadvantage.') : 
-  console.log(`Your enemy is smaller than you. You start with advantage.`);
-  return result;
-}
-
-function rollDoesHit(opponent) {
-    return opponent.armorClass < Math.floor(Math.random() * 21 - modifier);
-}
-
-function attack(attacker, opponent) {
-    if(rollDoesHit(opponent)) {
-      let damageRoll = Math.floor(Math.random() * attacker.attackPower);
-      opponent.health -= damageRoll;
-      console.log(`${attacker.displayTitle} has hit for ${damageRoll}`);
-      console.log('Health: ', opponent.health);
-      if (opponent.health <= 0) {
-        console.log(`${opponent.displayTitle} has been slain!`);
-      }
-    } else {
-      console.log(`${attacker.displayTitle} has missed.`)
-    }
-};
-
-let playerGoesFirst = determineInitiative();
-let attacker = playerGoesFirst ? player : enemy;
-let opponent = playerGoesFirst ? enemy : player;
-
-function handleAttack () {
-  attack(attacker, opponent);
-  let temp = opponent;
-  opponent = attacker;
-  attacker = temp;
-}
-
-const timer = setInterval(function () {
-  handleAttack();
-  if(attacker.health < 0 || opponent.health < 0){
-    clearInterval(timer);
+function rollDice(dice) {
+  const regexp = /(?<num>[0-9])(?:d)(?<sided>[0-9]*)/;
+  let {groups: {num, sided}} = regexp.exec(dice);
+  num = parseInt(num);
+  sided = parseInt(sided);
+  for (let i=0; i < num; i++){
+    roll += Math.floor( (Math.random() * sided) + 1 );
   }
-}, 1000);
+  return roll;
+}
+function rollToHit(opponent) {
+  rollDice('1d20');
+  if (roll === 20 ) {
+    console.log('critical hit');
+  }
+  else if ((roll - modifier) >= opponent.ac){
+    console.log(roll, 'hit');
+  } else {
+    console.log(roll, 'miss')
+  }
+}
+rollToHit(enemy);
+
+function initiative() {
+  if (player.dex > enemy.dex ) {
+    attacker = player;
+    defender = enemy;
+  } else {
+    attacker = enemy;
+    defender = player;
+  }
+  console.log('attacker: ', attacker);
+  console.log('defender: ', defender);
+}
+
+function attack(attacker, defender) {
+  console.log(attacker, defender);
+}
+
+// const attack = function(attacker, opponent) {
+//     if(rollDoesHit(opponent)) {
+//       let damageRoll = Math.floor(Math.random() * attacker.attackPower);
+//       opponent.health -= damageRoll;
+//       console.log(`${attacker.displayTitle} has hit for ${damageRoll}`);
+//       console.log('Health: ', opponent.health);
+//       if (opponent.health <= 0) {
+//         console.log(`${opponent.displayTitle} has been slain!`);
+//       }
+//     } else {
+//       console.log(`${attacker.displayTitle} has missed.`)
+//     }
+// };
+
+// let playerGoesFirst = determineInitiative();
+// let attacker = playerGoesFirst ? player : enemy;
+// let opponent = playerGoesFirst ? enemy : player;
+
+// function handleAttack () {
+//   attack(attacker, opponent);
+//   let temp = opponent;
+//   opponent = attacker;
+//   attacker = temp;
+// }
+
+// const timer = setInterval(function () {
+//   handleAttack();
+//   if(attacker.health < 0 || opponent.health < 0){
+//     clearInterval(timer);
+//   }
+// }, 1000);
 
 
 
