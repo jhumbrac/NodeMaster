fe221dd5929cdee26f75a8fb76822aad9ddfccc0
Michael Robil <ch.michaelsamir@hotmail.com>
2019-12-29 20:22:10 -0600

    monster api added

--- public/js/index.js
+++ public/js/index.js
@@ -136,4 +136,23 @@ $(document).ready(function () {
          window.location.replace('game.html');
       });
    });
+   let random = Math.floor(Math.random() * 200); 
+ $.ajax({
+   url: "http://www.dnd5eapi.co/api/monsters/" + random,
+   method: "GET"
+ }).then(function(res) {
+   const selectedMonster = {
+         name: res.name,
+         str: res.strength,
+         dex: res.dexterity,
+         con: res.constitution,
+         int: res.intelligence,
+         wis: res.wisdom,
+         chs: res.charisma,
+         hp: res.hit_points,
+         ac: res.armor_class,
+         img: "../img/jpeg"
+      };
+   console.log("The Monster is " + JSON.stringify(selectedMonster));
+ });
 });
