8bddd7a0d6496c7ca02de12049eac7e6d762a699
jhumbrac <john.humbracht@generalassemb.ly>
2019-12-31 16:07:17 -0600

    added images and music

--- public/js/index.js
+++ public/js/index.js
@@ -1,22 +1,32 @@
 $(document).ready(function () {
   let enemy = {};
-  let player = {
-    // ac: 5,
-    // dex: 15,
-    // attackPower: 15,
-    // hp: 7,
-    // name: 'Hero'
-  }; // need to set player = Hero;
-
+  let player = {};
   let attacker = player;
   let defender = enemy;
-  let hit = false;  // should these be done using a singular object? destructuring? https://2ality.com/2014/06/es6-multiple-return-values.html
-  let critHit = false;
   let save = false;
   let opponent; // don't remember what I was thinking here?
   let modifier = 0;
   let tempDice = '1d8';
   let roll = 0;
+
+
+  // battle command variables
+  let msg;
+  let options;
+  let fight = `<label>
+    <input id="fight" type="radio" class="nes-radio" name="answer" checked />
+    <span>Fight</span>
+    </label>
+    <label>
+    <input id="run" type="radio" class="nes-radio" name="answer" checked />
+    <span>Run</span>
+    </label>`;
+  let roomScore = $('#roomScore');
+  let battleScore = $('#battleScore');
+  let randomMonster = Math.floor(Math.random() * 325);
+  function fightMsg(msg) {
+    return $(`<p>${msg}</p>`);
+  }
 
   function rollDice(dice) {
     const regexp = /(?<num>[0-9])(?:d)(?<sided>[0-9]*)/;
@@ -37,33 +47,13 @@ $(document).ready(function () {
       return save = false;
     }
   };
-
-  // function rollToHit(opponent) {
-  //   rollDice('1d20');
-  //   if (roll === 20 ) {
-  //     critHit = true;
-  //     hit = true;
-  //     opponent.hp -= (roll * 2);
-  //   } else if ((roll - modifier) >= opponent.ac){
-  //     hit = true;
-  //     critHit = false;
-  //   } else {
-  //     hit = false;
-  //     critHit = false;
-  //   }
-  // };
-  function endGame() {
-    console.log('game has ended');
-  };
   function endBattle() {
     console.log('monster has been defeated');
   }
   function isAlive(opponent) {
-    console.log(`opponent: ${opponent.name}, player: ${player.name}`);
     if (opponent.hp > 0) {
       console.log(`${opponent.name}'s hp: ${opponent.hp}`);
-    }
-    else {
+    } else {
       if (opponent === player) {
         console.log(`${player.name} has been slain`);
         endGame();
@@ -75,7 +65,6 @@ $(document).ready(function () {
     }
   };
   function rollToHit(opponent) {
-    console.log(opponent, player);
     rollDice('1d20');
     if (roll === 20) {
       rollDice(tempDice);
@@ -90,17 +79,18 @@ $(document).ready(function () {
     } else {
       console.log(`miss`)
     }
-    if (opponent.name === player.name ){ 
-      console.log('torgar was hit', opponent.hp)
+    if (opponent.name === player.name) {
       $('#heroHp').attr('value', opponent.hp);
-      if ( (parseInt($('#heroHp').attr('value')) / parseInt($('#heroHp').attr('max'))) < .25 ) {
+      if ((parseInt($('#heroHp').attr('value')) / parseInt($('#heroHp').attr('max'))) < .25) {
         $('#heroHp').attr('class', "nes-progress is-error");
       }
     } else {
-      //$('#monsterHP').attr('value', opponent.hp);
-      console.log('monster was hit but we dont have a bar')
+      $('#monsterHp').attr('value', opponent.hp).attr('max', opponent.maxHp);
+      if ((parseInt($('#monsterHp').attr('value')) / parseInt($('#monsterHp').attr('max'))) < .25) {
+        $('#monsterHp').attr('class', "nes-progress is-error");
+      }
     }
-    
+
   };
   function initiative() {
     if (player.dex > enemy.dex) {
@@ -116,21 +106,78 @@ $(document).ready(function () {
     rollToHit(defender);
     rollToHit(attacker);
   };
-
-  // function startBattle(){
-  //   //fight
-
-  //   attack(player, enemy);
-
-  //   //run
-  //   endGame();
-  // }
+  function emptyRoom() {
+    msg = "You find yourself in an empty room. You can't remember how you got here, and you have no idea where 'here' is.";
+    options = `<label>
+    <input id="lookAround" type="radio" class="nes-radio" name="answer" checked />
+    <span>Look Around</span>
+    </label>
+    <label>
+    <input id="openDoor" type="radio" class="nes-radio" name="answer" checked />
+    <span>Open Door</span>
+    </label>`;
+    $('#textBlock').html('');
+    $('#textBlock').append(fightMsg(msg)).append(options);
+  };
+  emptyRoom();
+  function startBattle() {
+    // msg = `The ${enemy.name} is upon you, prepare for battle!`;
+    msg = 'What do you want to do?'; // randomly have messages that describe the action - things like 'you circle the enemy looking for an opportunity' 
+    $('#textBlock').html('');
+    $('#textBlock').append(fightMsg(msg)).append(fight);
+  };
+  function endGame() {
+    msg = `You flee the battle. You are a coward`;
+    $('#textBlock').html(fightMsg(msg));
+    $('#game').html('');
+    $('#game').attr('class', 'dead').append($('<h1>You have died</h1>'));
+  };
+  function encounter() {
+    $.ajax({
+      url: "http://www.dnd5eapi.co/api/monsters/" + randomMonster,
+      method: "GET"
+    }).then(function (res) {
+      enemy = {
+        name: res.name,
+        str: res.strength,
+        dex: res.dexterity,
+        con: res.constitution,
+        int: res.intelligence,
+        wis: res.wisdom,
+        chs: res.charisma,
+        hp: res.hit_points,
+        maxHp: res.hit_points,
+        ac: res.armor_class,
+        //img: `../img/${res.type}.png`
+        img: `../img/orc.png`
+      };
+      console.log("The Monster is " + JSON.stringify(enemy));
+      msg = `The ${enemy.name} is upon you, prepare for battle!`;
+      let $monster = `<div class="monster">
+      <img src="${enemy.img}" alt="${enemy.name} image">
+      <div class="monsterStats">
+        <h2>${enemy.name}</h2>
+        <p>HP: <progress id="monsterHp" value="${enemy.hp}" max="${enemy.hp}" class="nes-progress is-success"></p>
+      </div>`;
+      $('#textBlock').html('');
+      $('#roomDisplay').append($monster);
+      $('#textBlock').append(fightMsg(msg)).append(fight);
+      battleScore[0].play();
+      return enemy;
+    });
+  };
   $(document).on('click', '#fight', event => {
     event.preventDefault();
     attack(player, enemy);
+    // $('#textBlock').append(`<p>${player.name}</p><p>${enemy.name} was hit for</p>`); need to display results of battle
+    startBattle();
+  });
+  $(document).on('click', '#run', event => {
+    event.preventDefault();
+    endGame();
   })
   $(document).on("keydown", event => {
-    if (event.which == 13)
+    if (event.which == 13) // enter key
       $('body').addClass('charSelect');
   });
   $(document).on('click', '.selectChar', function (event) {
@@ -142,46 +189,37 @@ $(document).ready(function () {
       window.location.replace('game.html');
     });
   });
-  let random = Math.floor(Math.random() * 200);
-  $.ajax("/api/selectedChar", {
-    type: "GET"    
-  }).then(res=>{
-      player = {
-        name: res.name,
-        str: res.str,
-        dex: res.dex,
-        con: res.con,
-        int: res.int,
-        wis: res.wis,
-        chs: res.chs,
-        hp: res.hp,
-        ac: res.ac,
-        img: "../img/jpeg"
-      }
-    console.log(player)
+  $(document).on('click', '#lookAround', event=>{
+    msg = 'You find yourself in an empty room. Who lit these torches you wonder?';
+    $('#textBlock').html('');
+    $('#textBlock').append(fightMsg(msg));
+    emptyRoom();
   });
-  $.ajax({
-    url: "http://www.dnd5eapi.co/api/monsters/" + random,
-    method: "GET"
-  }).then(function (res) {
-    enemy = {
+  $(document).on('click', '#openDoor', event=>{
+    roomScore[0].pause();
+    encounter();
+  });
+
+  $.ajax("/api/selectedChar", {
+    type: "GET"
+  }).then(res => {
+    player = {
       name: res.name,
-      str: res.strength,
-      dex: res.dexterity,
-      con: res.constitution,
-      int: res.intelligence,
-      wis: res.wisdom,
-      chs: res.charisma,
-      hp: res.hit_points,
-      ac: res.armor_class,
+      str: res.str,
+      dex: res.dex,
+      con: res.con,
+      int: res.int,
+      wis: res.wis,
+      chs: res.chs,
+      hp: res.hp,
+      ac: res.ac,
       img: "../img/jpeg"
-    };
-    console.log("The Monster is " + JSON.stringify(enemy));
+    }
+    console.log(player);
+    return player;
   });
 });
 
-
-// create monster display
-
-// functions to start new battle
 // functions to end battle
+
+//saving throw
